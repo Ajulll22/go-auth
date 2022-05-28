a27c25ac229cc21ca5c9e74c2c88f4a36deb1716
Ajulll22 <ajulrizki@gmail.com>
2022-05-28 22:25:03 +0700

    fiber inisialisasi

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go-auth/database"
+	"go-auth/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":3000")
+}

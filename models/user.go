66d89cc92df412ff2c23da5f6534fe5e249fbe0b
Ajulll22 <ajulrizki@gmail.com>
2022-05-29 22:50:41 +0700

    model user

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	Id       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"type:varchar(100)"`
-	Email    string `gorm:"type:varchar(100);unique"`
-	Password []byte `gorm:"type:varchar(100)"`
+	Id       uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name" gorm:"type:varchar(100)"`
+	Email    string `json:"email" gorm:"type:varchar(100);unique"`
+	Password []byte `json:"-" gorm:"type:varchar(100)"`
 }
